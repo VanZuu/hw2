0ac90ead8f2539433ee399d1c4398ed4db90217d
VanZuu <triki1@yandex.ru>
2022-12-15 18:28:05 +0300

    первое задание

---
+++ HW3/task1.py
@@ -0,0 +1,15 @@
+#3.10 Вводим с клавиатуры десятичное число. Необходимо перевести его в шестнадцатиричную систему счисления.
+
+def convert_to_hexademical(num):
+    ans = ''
+    letters = ['a', 'b', 'c', 'd', 'e', 'f']
+    while num > 0:
+        char = num % 16
+        if char < 10: ans = str(char) + ans
+        else:
+            ans = letters[char - 10] + ans
+        num //= 16
+    return ans
+
+num = int(input('Введите число: '))
+print(convert_to_hexademical(num))
